c93aa6cc96a1766824cab96f01c40730fec45d65
David Wu <davidcnwu@gmail.com>
2013-01-12 13:11:14 -0800

    optimized SimpleDictionary to use a hash for fast lookup

--- lib/rscramblesolver/dictionary/simple_dictionary.rb
+++ lib/rscramblesolver/dictionary/simple_dictionary.rb
@@ -17,9 +17,24 @@ module Rscramblesolver
       end
 
       def real_word?(potential_word)
-        word_list.include?(potential_word)
+        word_hash[potential_word.length].include?(potential_word)
       end
 
+      private
+        require 'set'
+
+        def word_hash
+          @word_hash ||= word_hash_internal
+        end
+
+        # Creates a hash from word length to a set of words with that word length
+        def word_hash_internal
+          hash = Hash.new(Set.new)
+          word_list.each do |word|
+            hash[word.length].add(word)
+          end
+          return hash
+        end
     end
 	end
 end
