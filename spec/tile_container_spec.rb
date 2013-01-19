efe1149fedc402fde76c76122fb7dc2a27ec7c20
David Wu <davidcnwu@gmail.com>
2013-01-18 17:55:58 -0800

    added scoring for words

--- spec/tile_container_spec.rb
+++ spec/tile_container_spec.rb
@@ -32,4 +32,49 @@ describe TileContainer do
       new_tilecontainer.contents.size.should == tilecontainer.contents.size + 1
     end
   end
+
+  describe :score do
+    it 'should respond to score' do
+      TileContainer.new.should respond_to(:score)      
+    end
+
+    it 'should calculate score correctly' do
+      a_tile = create_tile('A', 1, 1)
+      r_tile = create_tile('R', 2, 3)
+      t_tile = create_tile('T', 1, 2)
+
+      tilecontainer = TileContainer.new([a_tile, r_tile, t_tile])
+
+      tilecontainer.score.should == 24
+    end
+
+  end
+
+  describe :to_word do
+    it 'should respond to to_word' do
+      TileContainer.new.should respond_to(:to_word)
+    end
+
+    it 'should return the right word' do
+      a_tile = create_tile('A', 1, 1)
+      r_tile = create_tile('R', 2, 3)
+      t_tile = create_tile('T', 1, 2)
+
+      tilecontainer = TileContainer.new([a_tile, r_tile, t_tile])
+
+      tilecontainer.to_word.should == 'ART'
+    end
+
+  end
+
+  def create_tile(letter, points, multiplier)
+    return Tile.new(
+        tileattributes: TileAttributes.new(
+            letter: letter,
+            points: points,
+            multiplier: multiplier
+          )
+      )
+  end
+
 end
