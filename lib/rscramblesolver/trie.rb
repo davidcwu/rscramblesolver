f006b9f6e4c604f22d32282e48d1696f8bb2d52e
David Wu <davidcnwu@gmail.com>
2013-02-21 12:08:03 -0800

    adding feature code for a trie

--- lib/rscramblesolver/trie.rb
+++ lib/rscramblesolver/trie.rb
@@ -1,12 +1,66 @@
 module Rscramblesolver
   class Trie
-    def initialize
+    attr_reader :root
+
+    def initialize()
+      @root = TrieBaseNode.new
     end
 
-    def add
+    # TODO: find a way to make this more elegant
+    def add(item)
+      item.each_char.inject(root) do |node, component|
+        next_node = node.children[component]
+
+        unless next_node
+          node.children[component] = TrieNode.new(
+              :value => component,
+            )
+        end
+
+      end
+
+      return self
     end
 
-    def valid_prefix?
+    # TODO: find a way to make this more elegant
+    def valid_prefix?(item)
+      item.each_char.inject(root) do |node, component|
+        next_node = node.children[component]
+
+        if next_node
+          node = next_node
+        else
+          return false
+        end
+      end
+
+      return true
     end
   end
+
+  # Consider pulling these classes out into a separate class
+  class TrieBaseNode
+    attr_reader :children
+
+    def initialize(args = {})
+      @children = args[:children] || {}
+    end
+
+    # Kind of hacky
+    def root?
+      @value.nil?
+    end
+  end
+
+  # Use inheritance sparingly
+  class TrieNode < TrieBaseNode
+    attr_reader :value
+
+    def initialize(args = {})
+      super(args)
+      @value = args[:value]
+    end
+
+  end
+
 end
--- spec/trie_spec.rb
+++ spec/trie_spec.rb
@@ -23,4 +23,19 @@ describe Trie do
       trie.should respond_to(:valid_prefix?)
     end
   end
+
+  describe 'add and then calling valid_prefix?' do
+    it 'should be successful with the same word' do
+      trie.add('hello').valid_prefix?('hello').should be_true
+    end
+
+    it 'should be successful with a prefixed version of the added word' do
+      trie.add('ballerina').valid_prefix?('ball').should be_true
+    end
+
+    it 'should be unsuccessful with a different prefix' do
+      trie.add('carpinteria').valid_prefix?('carpet').should be_false
+    end
+  end
+
 end
